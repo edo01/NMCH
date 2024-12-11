9315868c91ce10320e782b4936f854d31c79d062
edo01 <edocarra012@gmail.com>
2024-12-11 11:01:53 +0100

    Add documentation to NMCH.hpp

--- include/NMCH/methods/NMCH.hpp
+++ include/NMCH/methods/NMCH.hpp
@@ -1,48 +1,5 @@
-#ifndef NMCH_CUH
-#define NMCH_CUH
-
-/**
- * ## Monte Carlo Simulation of Heston Model
- * 
- * The Heston model for asset pricing has been widely examined in the literature. Under this model, the dynamics of the asset 
- * price $S_t$ and the variance $v_t$ are governed by the following system of stochastic differential equations:
- * 
- * $$
- * dS_t = r S_t \, dt + \sqrt{v_t} S_t \, dZ_t
- * $$
- * $$
- * dv_t = \kappa (\theta - v_t) \, dt + \sigma \sqrt{v_t} \, dW_t
- * $$
- * $$
- * Z_t = \rho W_t + \sqrt{1 - \rho^2} Z_t
- * $$
- * 
- * Where:
- * - The spot values $S_0 = 1$ and $v_0 = 0.1$,
- * - $r$ is the risk-free interest rate, assumed to be $r = 0$,
- * - $\kappa$ is the mean reversion rate of the variance,
- * - $\theta$ is the long-term variance,
- * - $\sigma$ is the volatility of variance,
- * - $W_t$ and $Z_t$ are independent Brownian motions.
- * 
- * In this project, we aim to compare two distinct methods for simulating an at-the-money call option (where "at-the-money" here means $K = S_0 = 1$) at maturity $T = 1$ under the Heston model. The option has a payoff given by $f(x) = (x - K)^+$, so we want to simulate with Monte Carlo the expectation $E[f(S_T)] = E[(S_1 - 1)^+]$. 
- * 
- * This comparison will focus on the efficiency and accuracy of each simulation method in pricing the call option within the stochastic volatility framework of the Heston model.
- * 
- * We begin with the Euler discretization scheme, which updates the asset price $S_t$ and the volatility $v_t$ at each time step as follows:
- * 
- * $$
- * S_{t+\Delta t} = S_t + r S_t \Delta t + \sqrt{v_t} S_t \sqrt{\Delta t} \left( \rho G_1 + \sqrt{1 - \rho^2} G_2 \right)
- * $$
- * 
- * $$
- * v_{t+\Delta t} = g \left( v_t + \kappa (\theta - v_t) \Delta t + \sigma \sqrt{v_t} \sqrt{\Delta t} G_1 \right)
- * $$
- * 
- * where $G_1$ and $G_2$ are independent standard normal random variables, and the function $g$ is either taken to be equal to $(\cdot)^+$ or to $|\cdot|$.
- * 
- */
-
+#ifndef NMCH_HPP
+#define NMCH_HPP
 
 
 #include <stdio.h>
@@ -55,6 +12,13 @@ namespace nmch::methods
      * NMCH is the base abstract class for giving the interface of different optimization strategies.
      * All the NMCH methods should have the same structure: the user creates a new NMCH object,
      * initializes it, computes the price of the option and the volatility at time T, and finalizes it.
+     *
+     * If the one wants to create a new implmentation, he should inherit from this class and implement the
+     * necessary methods. 
+     *
+     * If the user wants to use the same method with different parameters, he can use the set methods and
+     * call multiple times the compute method avoiding the initialization and finalization of the memory,
+     * since the random states are kept in the memory.
      * 
      * Using this interface, it is easier to compare different methods and to create new ones, optimizing 
      * different aspects of the computation: memory management, shared memory, etc.
@@ -106,15 +70,8 @@ namespace nmch::methods
             float get_variance() const { return variance; }
 
             /**
-             * @return the bias
+             * Set methods for the exploration of the parameters
              */
-            float get_bias() const 
-            { 
-                float real_price = this->S_0 * nmch::utils::NP((this->r + 0.5 * this->sigma * this->sigma)/this->sigma) -
-                                        this->K * expf(-this->r) * nmch::utils::NP((this->r - 0.5 * this->sigma * this->sigma) /
-                                        this->sigma);
-                return abs((this->strike_price - real_price));
-            }
 
             void set_k(float k) { this->k = k; }
 
@@ -160,4 +117,4 @@ namespace nmch::methods
 } // namespace nmch::methods
 
 
-#endif // NMCH_CUH
+#endif // NMCH_HPP
